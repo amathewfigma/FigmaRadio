3e2d995cf5c746388f869a15865d552bc060b9f8
Arun <amathew@figma.com>
2024-06-21 14:45:01 -0400

    Figma Radio App

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Player from './Player.tsx';
+
+function App() {
+  return <Player />;
+}
+
+export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import {
+  Button as BaseButton,
+  ButtonOwnerState,
+  ButtonProps
+} from "@mui/base/Button";
+
+const Button = React.forwardRef(function Button(
+  props: ButtonProps,
+  ref: React.ForwardedRef<HTMLButtonElement>
+) {
+  return (
+    <BaseButton
+      {...props}
+      slotProps={{
+        root: (state: ButtonOwnerState) => ({
+          className: `hover:text-orange-500 transition-colors ${
+            state.focusVisible ? "outline-0 ring-2 ring-orange-500" : ""
+          }`
+        })
+      }}
+      ref={ref}
+    />
+  );
+});
+
+export default Button;
